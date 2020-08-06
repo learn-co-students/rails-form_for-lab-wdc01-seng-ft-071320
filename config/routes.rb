6c62dc624362e0bc078cdc3ebde3461a92a38da5
slee713 <steven.lee1393@gmail.com>
2020-08-06 17:10:04 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :school_classes, except: [:destroy]
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
